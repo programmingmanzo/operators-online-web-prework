d8d32b4a303580c5b04617b1175e33c399b945fc
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-19 06:52:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -14,7 +14,7 @@ def not_safe?(speed)
     true
     elsif speed < 40
     true
-    elsif (speed > 40) && (speed < 60) ? !true : true  
+    elsif (speed > 40) && (speed < 60) ? == false : == true  
   end
 end
 	
